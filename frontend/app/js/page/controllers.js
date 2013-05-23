a7e468c79ef82d40f2cd1dbb62bdc308e5a76f22
Jin <jinwyp@gmail.com>
2013-05-23 18:08:05 +0800

    提交angularjs

---
+++ frontend/app/js/page/controllers.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* Controllers */
+function PagesController($scope, $http, $routeParams) {
+
+
+    $scope.pages = [
+        { siteid:1, pagename:'Homepage', pageid:101, pageorder:1, pagelayoutid:10, pageblocks:[] },
+        { siteid:1, pagename:'Channel2', pageid:102, pageorder:2, pagelayoutid:10, pageblocks:[] },
+        { siteid:1, pagename:'Channel3', pageid:103, pageorder:3, pagelayoutid:10, pageblocks:[] }
+    ];
+
+}
+
+//PhoneListCtrl.$inject = ['$scope', '$http'];
+//function PhoneDetailCtrl($scope, $routeParams, $http) {
+//    $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+//        $scope.phone = data;
+//    });
+//}
+
+//PhoneDetailCtrl.$inject = ['$scope', '$routeParams'];
