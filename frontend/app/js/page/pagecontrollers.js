aac761d8ce3e7865c8ef1e64a34d753058c2d0b0
Jin <jinwyp@gmail.com>
2013-05-29 13:35:37 +0800

    提交未完成

--- frontend/app/js/page/pagecontrollers.js
+++ frontend/app/js/page/pagecontrollers.js
@@ -1,6 +1,38 @@
 'use strict';
 
-var pageapp = angular.module('pagemodule', []);
+var pageapp = angular.module('pagemodule', ['ui']);
+
+/*
+pageapp.directive('anyKeypress', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attr, ctrl) {
+            element.bind('keypress', function(){
+                scope.$apply(function(s) {
+                    s.$eval(attr.zKeypress);
+                });
+            });
+        }
+    };
+});
+*/
+
+/*
+
+pageapp.directive('enterKeypress', function(){
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if(event.which === 13) {
+                scope.$apply(function(){
+                    scope.$eval(attrs.onEnter);
+                });
+                event.preventDefault();
+            }
+        });
+    };
+});
+*/
+
 
 var page = {
     m:{},
@@ -9,52 +41,108 @@ var page = {
 
 pageapp.controller(page.c);
 
+pageapp.factory('modelSite', function(){
 
+    var sitedata = {
+        siteid:1,
+        sitename:'NewSite',
 
-
-/* Controllers */
-page.c.Pagelist = function($scope, $location, $http, $routeParams) {
-
-    var pagesdata = [
-        { siteid:1, pagename:'Homepage', pageid:101, pagetype:2, pagetitle:"Homepage", pageurl:"homepage",  pageorder:1, pagelayoutid:10, pagelayoutdata:[
-                {layoutcontainerclass:"span9", layoutcontainerid:"layoutcontainer1" , blocks:[
+        pagelist : [
+            { siteid:1, pagename:'Homepage', pageid:101, pagetype:2, pagetitle:"Homepage", pageurl:"homepage",  pageorder:1, pagelayoutid:10,
+                pagelayoutdata:[
+                    {layoutcontainerclass:"span9", layoutcontainerid:"layoutcontainer1" , blocks:[
                         {blockid:100, blocktype:1, blockname:"name1" } ,
                         {blockid:101, blocktype:1, blockname:"name2" } ,
                         {blockid:102, blocktype:1, blockname:"name3" }
                     ]
-                },
-                {layoutcontainerclass:"span3", layoutcontainerid:"layoutcontainer2", blocks:[] }
-            ]
-        },
-        { siteid:1, pagename:'Channel2', pageid:102,  pagetype:2, pagetitle:"Ch2", pageurl:"ch2", pageorder:6, pagelayoutid:10, pagelayoutdata:[] },
-        { siteid:1, pagename:'Channel3', pageid:103,  pagetype:2, pagetitle:"Ch3", pageurl:"ch3", pageorder:10, pagelayoutid:10, pagelayoutdata:[] },
+                    },
+                    {layoutcontainerclass:"span3", layoutcontainerid:"layoutcontainer2", blocks:[] }
+                ]
+            },
+            { siteid:1, pagename:'Channel2', pageid:102,  pagetype:2, pagetitle:"Ch2", pageurl:"ch2", pageorder:6,  pagelayoutid:10, pagelayoutdata:[] },
+            { siteid:1, pagename:'Channel3', pageid:103,  pagetype:2, pagetitle:"Ch3", pageurl:"ch3", pageorder:10, pagelayoutid:10, pagelayoutdata:[] },
 
 
-        { siteid:1, pagename:'article', pageid:103,  pagetype:1, pagetitle:"article", pageurl:"article", pageorder:0, pagelayoutid:10, pagelayoutdata:[] }
+            { siteid:1, pagename:'Article', pageid:103, pagetype:1, pagetitle:"article", pageurl:"article", pageorder:0, pagelayoutid:10, pagelayoutdata:[] }
+        ],
+
+        defaulstSelectedPageIndex:0,
+        defaulstSelectedLayoutIndex:0,
+        pagefilterArticleType:{pagetype:1},
+        pagefilterListType:{pagetype:2}
+
+    };
+
+    var layoutdata = [
+        {layoutid: 10, layoutname: '两列1', layouttype : 1, layoutorder:1, layoutcss:'ico_layout_00', layoutimage:'app/img/layout_templete.png'},
+        {layoutid: 10, layoutname: '两列2', layouttype : 1, layoutorder:2, layoutcss:'ico_layout_01', layoutimage:'app/img/layout_templete_01.png'},
+        {layoutid: 10, layoutname: '三列1', layouttype : 0, layoutorder:3, layoutcss:'ico_layout_02', layoutimage:'app/img/layout_templete_02.png'}
     ];
 
+    var factory = {};
+    factory.getSite = function () {
+        return  sitedata;
+    };
 
+    factory.getPageList = function () {
+        return  sitedata.pagelist;
+    };
 
-    $scope.pagefilterarticle = {  pagetype:1};
-    $scope.pagefilterlist = {  pagetype:2};
+    factory.getSinglePage = function (pageid) {
+        return  sitedata.pagelist[0];
+    };
 
-    $scope.pages = pagesdata;
-    $scope.pagelayoutdata = pagesdata[0].pagelayoutdata;
-//    console.log($scope.pagelayoutdata);
+    factory.addSinglePage = function (pagedata) {
+        return  sitedata.pagelist.push(pagedata);
+    };
 
-//    console.log($location.path) ;
-
-    $scope.siteconfig = {selectedPageIndex : 0};    // left menu default selected page
-
-    $scope.clickpage = function(indexid) {
-        $scope.siteconfig = {selectedPageIndex :indexid};
+    factory.getLayoutList = function() {
+        return  layoutdata;
     }
 
-    $scope.cssdisplay = 'displaynone';
+    return factory;
+});
+
+
+
+
+/* Controllers */
+page.c.Pagelist = function($scope, $location, $http, $routeParams, modelSite) {
+    $scope.site = {};
+    $scope.pages = [];
+    $scope.singlepage = {};
+
+    initialize();
+
+    function initialize(){
+        $scope.site = modelSite.getSite();
+        $scope.pages = modelSite.getPageList();
+        $scope.singlepage =  modelSite.getSinglePage(0);
+
+        $scope.pagefilterarticle = $scope.site.pagefilterArticleType;
+        $scope.pagefilterlist = $scope.site.pagefilterListType;
+        $scope.defaultselectedpageindex = $scope.site.defaulstSelectedPageIndex;    // left menu default selected page
+
+        $scope.cssdisplay = false;    //添加page的输入框默认不显示
+    }
+
+
+//    console.log($scope.pagelayoutdata);
+//    console.log($location.path) ;
+
+
+    $scope.clickpage = function(indexid) {
+        $scope.defaultselectedpageindex = indexid;
+    }
+
     $scope.newpage ={};
 
-    $scope.clickaddpage = function() {
-        $scope.cssdisplay = '';
+    $scope.showaddpageinput = function() {
+        $scope.cssdisplay = true;       //添加page的输入框显示
+    }
+
+    $scope.addpage = function() {
+        $scope.cssdisplay = false;       //添加page的输入框显示
         $scope.pages.push ({siteid:1, pagename:$scope.newpage.pagename, pageid:103,  pagetype:2, pagetitle:$scope.newpage.pagename});
         console.log($scope.pages);
     }
@@ -63,23 +151,15 @@ page.c.Pagelist = function($scope, $location, $http, $routeParams) {
 
 
 
-
 page.c.Layoutlist = function($scope, $location, $http, $routeParams) {
-    var layoutlist = [
-        {layoutid: 10, layoutname: '两列1', layouttype : 1, layoutorder:1, layoutcss:'ico_layout_00', layoutimage:'app/img/layout_templete.png'},
-        {layoutid: 10, layoutname: '两列2', layouttype : 1, layoutorder:2, layoutcss:'ico_layout_01', layoutimage:'app/img/layout_templete_01.png'},
-        {layoutid: 10, layoutname: '三列1', layouttype : 0, layoutorder:3, layoutcss:'ico_layout_02', layoutimage:'app/img/layout_templete_02.png'}
-    ];
 
-    $scope.layoutlist1 = layoutlist;
-
-    $scope.siteconfig = {selectedLayoutIndex : 0};    // left menu default selected page
-
-    $scope.clicklayout = function(indexid) {
-        $scope.siteconfig = {selectedLayoutIndex :indexid};
-
-
-    }
+//    $scope.layoutlist1 = layoutlist;
+//
+//    $scope.siteconfig = {selectedLayoutIndex : 0};    // left menu default selected page
+//
+//    $scope.clicklayout = function(indexid) {
+//        $scope.siteconfig = {selectedLayoutIndex :indexid};
+//    }
 
 }
 
