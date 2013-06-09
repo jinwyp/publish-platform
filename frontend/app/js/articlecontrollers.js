17849b28fb3daa20b734621586575287452d40f9
Jin <jinwyp@gmail.com>
2013-06-09 18:17:20 +0800

    提交未完成

--- frontend/app/js/articlecontrollers.js
+++ frontend/app/js/articlecontrollers.js
@@ -3,32 +3,37 @@
 /* App Module */
 var articleapp =angular.module('articlemodule', []);
 
-pageapp.factory('modelArticle', function(){
+articleapp.factory('modelArticle', function(){
 
-    var articlelist = [
-        {  "id": 1000, "title": "今日新闻 multiple partial views in angularjs.", "contentbody": "", "status": "needreview",
-            "created": "04/02/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan",  "category": "Today", "categoryid":1000,
-            "tags": [{ "tagid":10000, "tagname":"computer" },{ "tagid":10001, "tagname":"videocard" }]
-        },
-
-        {  "id": 1001, "title": "昨日新闻 multiple partial views in angularjs.", "contentbody": "", "status": "needreview",
-            "created": "04/02/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan",  "category": "Today", "categoryid":1000,
-            "tags": [{ "tagid":10001, "tagname":"videocard" }]
-        },
-
-        {  "id": 1002, "title": "昨日新闻汇总 multiple partial views in angularjs.", "contentbody": "", "status": "needreview",
-            "created": "04/02/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan",  "category": "Today", "categoryid":1000,
-            "tags": [{ "tagid":10000, "tagname":"computer" }]
-        }
-    ];
-
-
+    var articlelist ;
     if(window.localStorage){
-        if (JSON.parse(localStorage.getItem("newData")) == null ){
+        if (JSON.parse(localStorage.getItem("articlesData")) == null ){
+            articlelist = [
+                {  "id": 1000, "title": "今日新闻 multiple partial views in angularjs.", "contentbody": "", "status": "needreview",
+                    "created": "04/02/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan",  "category": "Today", "categoryid":1000,
+                    "tags": [{ "tagid":10000, "tagname":"computer" },{ "tagid":10001, "tagname":"videocard" }]
+                },
 
+                {  "id": 1001, "title": "昨日新闻 multiple partial views in angularjs.", "contentbody": "", "status": "needreview",
+                    "created": "04/02/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan",  "category": "Today", "categoryid":1000,
+                    "tags": [{ "tagid":10001, "tagname":"videocard" }]
+                },
+
+                {  "id": 1002, "title": "昨日新闻汇总 multiple partial views in angularjs.", "contentbody": "", "status": "needreview",
+                    "created": "04/02/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan",  "category": "Today", "categoryid":1000,
+                    "tags": [{ "tagid":10000, "tagname":"computer" }]
+                }
+            ];
+        }else{
+            articlelist = JSON.parse(localStorage.getItem("articlesData"));
         }
-        sitedata.headerdata=JSON.parse(localStorage.getItem("newData")) == null ? [] : JSON.parse(localStorage.getItem("newData"));
     }
+
+    var factory = {};
+
+    factory.getArticleList = function () {
+        return  articlelist;
+    };
 
     return factory;
 });
@@ -37,32 +42,28 @@ var article = {
     m:{},
     c:{}
 };
+articleapp.controller(article.c);
 
 
-
-articleapp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-      when('/', {templateUrl: 'article_list_left.html',   controller: ArticleListCtrl}).
-      when('/articledetail/:id', {templateUrl: 'article_list_right.html', controller: ArticleDetailCtrl}).
-      otherwise({redirectTo: '/'});
+articleapp.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    $routeProvider.
+        when('/',                     {templateUrl: 'article_list_tpl.html',   controller: article.c.articleListCtr}).
+        when('/aritlcedetail/:id', {templateUrl: 'article.html',              controller: article.c.articleDetailCtrl}).
+        otherwise({redirectTo: '/'});
 }]);
 
 
 
 /* Controllers */
-article.c.articlelist = function ArticleListCtrl($scope, $http) {
-  $http.get('app/js/article.json').success(function(data) {
-    $scope.articledetail = data;
-  });
-
-  $scope.orderProp = 'age';
+article.c.articleListCtrl = function ($scope, modelArticle) {
+    $scope.articlesdata = modelArticle.getArticleList();
+    $scope.orderProp = 'age';
 }
 
 
-article.c.articlelist = function ArticleDetailCtrl($scope, $routeParams, $http) {
-  $http.get('app/js/' + $routeParams.articlelistId + '.json').success(function(data) {
-    $scope.articlelist = data;
-  });
+article.c.articleDetailCtrl = function ($scope, $routeParams, modelArticle,) {
+
 }
 
 
