186d6f3ae2062dc6501ab38ce1cb1ab35150aa89
Jin <jinwyp@gmail.com>
2013-06-13 18:10:32 +0800

    finish article detail

--- frontend/app/js/articlecontrollers.js
+++ frontend/app/js/articlecontrollers.js
@@ -3,11 +3,32 @@
 /* App Module */
 var articleapp =angular.module('articlemodule', ['ui.bootstrap']);
 
+articleapp.directive('ckEditor', function() {
+    return {
+        require: '?ngModel',
+        link: function(scope, elm, attr, ngModel) {
+            var ck = CKEDITOR.replace(elm[0]);
+
+            if (!ngModel) return;
+
+            ck.on('pasteState', function() {
+                scope.$apply(function() {
+                    ngModel.$setViewValue(ck.getData());
+                });
+            });
+
+            ngModel.$render = function(value) {
+                ck.setData(ngModel.$viewValue);
+            };
+        }
+    };
+});
+
 articleapp.factory('modelArticle', function(){
 
     var articlelist ;
     if(window.localStorage){
-        if (JSON.parse(localStorage.getItem("articlesData")) == null ){
+        if (JSON.parse(localStorage.getItem("articlesData")) == null || JSON.parse(localStorage.getItem("articlesData")).length == 0){
             articlelist = [
                 {  "id": 1000, "title": "今日新闻 multiple partial views in angularjs.", "contentbody": "", "status": "needreview",
                     "created": "04/01/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan", "clickcount":1023, "category": "Today", "categoryid":1000,
@@ -18,7 +39,7 @@ articleapp.factory('modelArticle', function(){
                 },
 
                 {  "id": 1001, "title": "昨日新闻 multiple partial views in angularjs.", "contentbody": "", "status": "needreview",
-                    "created": "04/03/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan", "clickcount":13, "category": "Today", "categoryid":1000,
+                    "created": "04/02/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan", "clickcount":13, "category": "Today", "categoryid":1000,
                     "tags": [
                         { "tagid":10003, "tagname":"财经" },
                         { "tagid":10004, "tagname":"黄金" },
@@ -35,8 +56,34 @@ articleapp.factory('modelArticle', function(){
                     ]
                 },
 
-                {  "id": 1002, "title": "昨日新闻汇总 multiple partial views in angularjs.", "contentbody": "", "status": "needreview",
-                    "created": "03/02/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan",  "clickcount":975, "category": "Today", "categoryid":1000,
+                {  "id": 1002, "title": "前日新闻汇总 multiple partial views in angularjs.", "contentbody": "", "status": "needreview",
+                    "created": "03/03/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan",  "clickcount":975, "category": "Today", "categoryid":1000,
+                    "tags": [
+                        { "tagid":10005, "tagname":"期货" },
+                        { "tagid":10006, "tagname":"白银" },
+                        { "tagid":10005, "tagname":"期货" },
+                        { "tagid":10006, "tagname":"白银" },
+                        { "tagid":10005, "tagname":"期货" },
+                        { "tagid":10006, "tagname":"白银" },
+                        { "tagid":10005, "tagname":"期货" },
+                        { "tagid":10006, "tagname":"白银" }
+                    ]
+                },
+                {  "id": 1002, "title": "星期一新闻汇总 multiple partial views in angularjs.", "contentbody": "", "status": "needreview",
+                    "created": "03/04/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan",  "clickcount":975, "category": "Today", "categoryid":1000,
+                    "tags": [
+                        { "tagid":10005, "tagname":"期货" },
+                        { "tagid":10006, "tagname":"白银" },
+                        { "tagid":10005, "tagname":"期货" },
+                        { "tagid":10006, "tagname":"白银" },
+                        { "tagid":10005, "tagname":"期货" },
+                        { "tagid":10006, "tagname":"白银" },
+                        { "tagid":10005, "tagname":"期货" },
+                        { "tagid":10006, "tagname":"白银" }
+                    ]
+                },
+                {  "id": 1002, "title": "星期二新闻汇总 multiple partial views in angularjs.", "contentbody": "", "status": "needreview",
+                    "created": "03/05/2013", "published": "04/21/2013",  "author": "Eric",  "editor": "iFan",  "clickcount":975, "category": "Today", "categoryid":1000,
                     "tags": [
                         { "tagid":10005, "tagname":"期货" },
                         { "tagid":10006, "tagname":"白银" },
@@ -64,6 +111,16 @@ articleapp.factory('modelArticle', function(){
         for(var i = articlelist.length; i--;){
             if (articlelist[i].id == articleid) {
                  return articlelist[i];
+            }
+        }
+    };
+
+    factory.delArticleById = function (articleid) {
+        for(var i = articlelist.length; i--;){
+            if (articlelist[i].id == articleid) {
+                articlelist.splice(i, 1);
+                localStorage.setItem("articlesData",JSON.stringify(articlelist));
+                return ;
             }
         }
     };
@@ -98,14 +155,36 @@ articleapp.controller.articleList = function ($scope, modelArticle) {
 
     $scope.clickArticle = function(article) {
         $scope.articledata = article;
-        console.log(article.id);
+    }
+
+    $scope.delArticle = function(articleid) {
+        modelArticle.delArticleById(articleid);
+        $scope.articledata = $scope.articlesdata[0];
+        console.log(articledata.id);
     }
 }
 
 
 articleapp.controller.articleDetail = function ($scope, $routeParams, modelArticle) {
+    $scope.cssTagsPanel = false;
+
     var articleId = $routeParams.articleId;
     $scope.articledata = modelArticle.getArticleById(articleId);
+
+    $scope.$on('$viewContentLoaded', function () {
+        console.log("$viewContentLoaded");
+    });
+
+    $scope.delArticle = function(articleid) {
+        modelArticle.delArticleById(articleid);
+        alert('Deleted');
+        $scope.articledata = modelArticle.getArticleList()[0];
+
+    }
+
+/*    $scope.showTagsPanel = function(articleid) {
+        $scope.cssTagsPanel = !$scope.cssTagsPanel;
+    }*/
 }
 
 articleapp.controller.articleSave = function ($scope, $routeParams, modelArticle) {
