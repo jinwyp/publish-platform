9c66f1f4433f0f296ffa240bd0ce422592d0e6bc
Jin <jinwyp@gmail.com>
2013-07-01 18:09:23 +0800

    add

--- frontend/app/js/pagecontrollers.js
+++ frontend/app/js/pagecontrollers.js
@@ -274,7 +274,7 @@ pageapp.factory('modelSite', function(){
 page.c.Pagelist = function($scope, $location, $http, $routeParams, $compile, modelSite) {
 
     function initialize(){
-
+        $('#tagsinput').tagsInput();
         $scope.newblock = {
             blockid : 100,
             blocktype : 1,
