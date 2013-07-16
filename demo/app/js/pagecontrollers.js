3cc1a3b708961a5cfca0f95c9b2043d6e348baea
susa <huangxiaoyan2007@yeah.net>
2013-07-16 18:16:17 +0800

    preview显示block

--- demo/app/js/pagecontrollers.js
+++ demo/app/js/pagecontrollers.js
@@ -16,26 +16,38 @@
 		.otherwise({redirectTo: '/index'});
 }]);*/
 
- page.c.GlobalCtrl = function($scope, modelSite){
+ page.c.GlobalCtrl = function($scope, modelSite, modelArticle){
+    //加载头部内容
     $scope.header = modelSite.getHeader();
 
+    //加载底部内容
     $scope.footer = modelSite.getfooter();
     $scope.footermaxindex=$scope.footer.length-1 < 0 ? 0 : $scope.footer.length-1;
     $scope.cssblocklayoutselected = 0;
 
+    //加载block内容
+     var site = modelSite.getSite();
+     for (var i=site.pagelist.length-1; i>=0; i--)
+     {
+         for (var j = site.pagelist[i].pagelayoutdata.length-1; j>=0; j--)
+         {
+             for (var k = site.pagelist[i].pagelayoutdata[j].blocks.length-1; k>=0; k--)
+             {
+                 if(site.pagelist[i].pagelayoutdata[j].blocks[k].blocktype == 'auto'){
+                     var articles = modelArticle.getArticlesByTags(site.pagelist[i].pagelayoutdata[j].blocks[k].blocktag, site.pagelist[i].pagelayoutdata[j].blocks[k].blockquantity, site.pagelist[i].pagelayoutdata[j].blocks[k].blockcategory);
+                     site.pagelist[i].pagelayoutdata[j].blocks[k].blockarticles = articles;
+                 }
+             }
+         }
+     }
+     $scope.pages = site.pagelist;
+     $scope.singlepage =  $scope.pages[0];
 
-	$scope.todayhot = function(item) {
-		return item.tag == '';
-    };
+     //链接页面
 
-	$scope.showCurrent = function(indexid) {
-		$scope.cssblocklayoutselected = indexid;
-	}
-
-	$scope.slideshow = function(param1){ 
-		$('#'+param1).slideBox();
-		return true;
-	}
+     $scope.showCurrent = function(currentpage){
+         $scope.singlepage = currentpage;
+     }
 }
 
 
