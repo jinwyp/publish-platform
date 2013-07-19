5e58b9470fef8b6a8060c805cfc77e6f91195e45
susa <huangxiaoyan2007@yeah.net>
2013-07-19 18:02:54 +0800

    template 10

--- frontend/ckeditor/plugins/templates/templates/default.js
+++ frontend/ckeditor/plugins/templates/templates/default.js
@@ -18,37 +18,43 @@ CKEDITOR.addTemplates("default",{
             title:"Example 2",
             image:"template2.gif",
             description:"",
-            html:'<div class="globalContBox" style=""><div class="imgGroupColumUL clearfix">'+
-                '<li><h6>Health Hazards At The Movies</h6><a href="#" class="linkImg"><img src="app/img/symptomfind/temp_12.jpg" alt="" /></a><p class="linkTxt">Signs and Symptoms Of Early Miscarriage</p></li>'+
-                '<li><h6>Getting A Flu Shot</h6><a href="#" class="linkImg"><img src="app/img/symptomfind/temp_13.jpg" alt="" /></a><p class="linkTxt">8 Germ-Filled Things In Your Home That Will Shock You</p></li>'+
-                '<li class="last"><h6>Taming Frizzy Hair</h6><a href="#" class="linkImg"><img src="app/img/symptomfind/temp_14.jpg" alt="" /></a><p class="linkTxt">Diabetic Eye Disease: How Diabetes Can Affect Your Vision</p></li>'+
-                '</div></div>'
+             html:'<div class="globalTitle GT_blue clearfix"><div class="arrow"></div><h6>TITLE</h6></div><div class="globalContBox">'+
+                 '<div class="UL_layoutbox clearfix"><div class="UL_layoutbox_left"><h6>Additional Information on Lactose Intolerance</h6>'+
+                 '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>'+
+                 '<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat</p>'+
+                 '<h6>Questions to Ask Your Doctor</h6>'+
+                 '<ul class="UL_layoutbox_ULColum clearfix">'+
+                 '<li><span>1.</span><p>Lorem ipsum dolor sit ame</p></li><li><span>2.</span><p>Lorem ipsum dolor sit amt</p></li>'+
+                 '<li><span>3.</span><p>Lorem ipsum dolor sit amt</p></li><li><span>4.</span><p>Lorem ipsum dolor sit amt</p></li>'+
+                 '<li><span>5.</span><p>Lorem ipsum dolor sit amt</p></li><li><span>6.</span><p>Lorem ipsum dolor sit amt</p></li>'+
+                 '</ul><a href="###" class="btn_PrintQuestions">Print Question Checklist</a></div>'+
+                 '<div class="UL_layoutbox_right"><h6>Related Articles on Lactose Intolerance</h6>'+
+                 '<ul class="horizontal_UL mini clearfix">'+
+                 '<li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_01.jpg" alt="" /></a></div>'+
+                 '<div class="rightList"><h6><a href="#">Guidelines For A Lactose Intolerance Diet</a></h6>'+
+                 '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam... <a href="###">more</a></p>'+
+                 '</div></li><li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_02.jpg" alt="" /></a></div>'+
+                 '<div class="rightList"><h6><a href="#">Non-Dairy Milk Substitutes</a></h6>'+
+                 '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam... <a href="###">more</a></p>'+
+                 '</div></li><li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_03.jpg" alt="" /></a></div>'+
+                 '<div class="rightList"><h6><a href="#">Organic Soy Milk</a></h6>'+
+                 '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam... <a href="###">more</a></p>'+
+                 '</div></li><li class="last"><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_04.jpg" alt="" /></a></div>'+
+                 '<div class="rightList"><h6><a href="#">Wheat Allergies</a></h6>'+
+                 '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam... <a href="###">more</a></p>'+
+                 '</div></li></ul></div></div></div>'
      },{
          title:'Example 3',
          image:'template3.gif',
          description:"",
-         html:'<div class="globalTitle GT_blue clearfix"><div class="arrow"></div><h6 class="blue">Img & Text</h6></div>'+
-             '<div class="globalContBox"><ul class="horizontal_UL">'+
-             '<li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_06.jpg" alt="" /></a></div>'+
-             '<div class="rightList"><h6><a href="#">10 Tips For Surviving A Public Restroom Visit</a></h6>'+
-             '<p>The public restroom is a delicate topic of conversation for many people. There are men who cringe at the near mention of a public urinal, while many women refuse to use public restrooms for anything aside from washing their hands. Here...<a href="###">more</a></p>'+
-             '</div></li><li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_07.jpg" alt="" /></a></div>'+
-             '<div class="rightList">'+
-             '<h6><a href="#">Reasons Why Your Eye Keeps Twitching</a></h6>'+
-             '<p>Almost everyone has experienced some form of eye spasm, often described as sporadic twitching under or around their eyes. The spasms can vary in strength and speed, commonly occurring around the upper eyelids, but sometimes occurring in... <a href="###">more</a></p>'+
-             '</div></li>'+
-             '<li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_08.jpg" alt="" /></a></div><div class="rightList">'+
-             '<h6><a href="#">Infrared Body Wrap For Reducing Fat And Cellulite</a></h6>'+
-             '<p>Many people are claiming they have lasting effects of weight loss, and are a safer alternative to cosmetic surgeries like liposuction for slimming down and eliminating cellulite. But the question still remains: Do these new infrared body...<a href="###">more</a></p>'+
-             '</div></li>'+
-             '<li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_09.jpg" alt="" /></a></div>'+
-             '<div class="rightList"><h6><a href="#">Insulin Injections: A Guide For Diabetes Treatment</a></h6>'+
-             '<p>Insulin is a hormone that plays a vital role in the body by regulating the amount of sugar in a person is bloodstream and storing any excess glucose for energy. When a person eats, carbohydrates are broken down into sugar that can be... <a href="###">more</a></p>'+
-             '</div></li>'+
-             '<li class="last"><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_10.jpg" alt="" /></a></div>'+
-             '<div class="rightList"><h6><a href="#">What Do Bleeding Gums Mean And How To Fix It</a></h6>'+
-             '<p>Bleeding gums can indicate more than just a problem with your oral hygiene habits. If left unchecked, poor dental health can lead to serious complications with your health, especially heart problems. <a href="###">more</a></p>'+
-             '</div></li></ul></div>'
+         html:'<div class="globalContBox"><div class="ParagraphArticle">'+
+             '<div class="quote_largespace"><div class="quoteboard"><div class="icon_topright"></div><div class="icon_bottomleft"></div>'+
+             '<div class="quote_board clearfix"><div class="qb_contbox"><h3>What can I eat?</h3>'+
+             '<p><b>Eat or drink other foods instead of milk and milk products. </b>ou can substitute soy milk and soy cheese for milk and milk products. You can also use nondairy creamers in your coffee. But keep in mind that nondairy creamers do not contain the same vitamins and minerals as milk, and they may contain more fat than milk contains.</p>'+
+             '</div><div class="qb_contbox havedot"><h3>Foods to Avoid</h3><p><b>Broccoli, okra, kale, collards, and turnip greens</b></p>'+
+             '<p><b>Canned sardines, tuna, and salmon</b></p><p><b>Calcium-fortified juices and cereals</b></p>'+
+             '<p><b>Calcium-fortified soy products such as soy milk, tofu, and soybeans</b></p><p><b>Almonds</b></p>'+
+             '</div></div></div></div></div></div>'
      },{
          title:'Example 4',
          image:'template4.gif',
@@ -139,42 +145,36 @@ CKEDITOR.addTemplates("default",{
          title:'Example 9',
          image:'template1.gif',
          description:'',
-         html:'<div class="globalContBox"><div class="ParagraphArticle">'+
-             '<div class="quote_largespace"><div class="quoteboard"><div class="icon_topright"></div><div class="icon_bottomleft"></div>'+
-             '<div class="quote_board clearfix"><div class="qb_contbox"><h3>What can I eat?</h3>'+
-             '<p><b>Eat or drink other foods instead of milk and milk products. </b>ou can substitute soy milk and soy cheese for milk and milk products. You can also use nondairy creamers in your coffee. But keep in mind that nondairy creamers do not contain the same vitamins and minerals as milk, and they may contain more fat than milk contains.</p>'+
-             '</div><div class="qb_contbox havedot"><h3>Foods to Avoid</h3><p><b>Broccoli, okra, kale, collards, and turnip greens</b></p>'+
-             '<p><b>Canned sardines, tuna, and salmon</b></p><p><b>Calcium-fortified juices and cereals</b></p>'+
-             '<p><b>Calcium-fortified soy products such as soy milk, tofu, and soybeans</b></p><p><b>Almonds</b></p>'+
-             '</div></div></div></div></div></div>'
+         html:'<div class="globalTitle GT_blue clearfix"><div class="arrow"></div><h6 class="blue">Img & Text</h6></div>'+
+             '<div class="globalContBox"><ul class="horizontal_UL">'+
+             '<li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_06.jpg" alt="" /></a></div>'+
+             '<div class="rightList"><h6><a href="#">10 Tips For Surviving A Public Restroom Visit</a></h6>'+
+             '<p>The public restroom is a delicate topic of conversation for many people. There are men who cringe at the near mention of a public urinal, while many women refuse to use public restrooms for anything aside from washing their hands. Here...<a href="###">more</a></p>'+
+             '</div></li><li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_07.jpg" alt="" /></a></div>'+
+             '<div class="rightList">'+
+             '<h6><a href="#">Reasons Why Your Eye Keeps Twitching</a></h6>'+
+             '<p>Almost everyone has experienced some form of eye spasm, often described as sporadic twitching under or around their eyes. The spasms can vary in strength and speed, commonly occurring around the upper eyelids, but sometimes occurring in... <a href="###">more</a></p>'+
+             '</div></li>'+
+             '<li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_08.jpg" alt="" /></a></div><div class="rightList">'+
+             '<h6><a href="#">Infrared Body Wrap For Reducing Fat And Cellulite</a></h6>'+
+             '<p>Many people are claiming they have lasting effects of weight loss, and are a safer alternative to cosmetic surgeries like liposuction for slimming down and eliminating cellulite. But the question still remains: Do these new infrared body...<a href="###">more</a></p>'+
+             '</div></li>'+
+             '<li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_09.jpg" alt="" /></a></div>'+
+             '<div class="rightList"><h6><a href="#">Insulin Injections: A Guide For Diabetes Treatment</a></h6>'+
+             '<p>Insulin is a hormone that plays a vital role in the body by regulating the amount of sugar in a person is bloodstream and storing any excess glucose for energy. When a person eats, carbohydrates are broken down into sugar that can be... <a href="###">more</a></p>'+
+             '</div></li>'+
+             '<li class="last"><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_10.jpg" alt="" /></a></div>'+
+             '<div class="rightList"><h6><a href="#">What Do Bleeding Gums Mean And How To Fix It</a></h6>'+
+             '<p>Bleeding gums can indicate more than just a problem with your oral hygiene habits. If left unchecked, poor dental health can lead to serious complications with your health, especially heart problems. <a href="###">more</a></p>'+
+             '</div></li></ul></div>'
      },{
          title:'Example 10',
          image:'template2.gif',
          description:'',
-         html:'<div class="globalTitle GT_blue clearfix"><div class="arrow"></div><h6>TITLE</h6></div><div class="globalContBox">'+
-             '<div class="UL_layoutbox clearfix"><div class="UL_layoutbox_left"><h6>Additional Information on Lactose Intolerance</h6>'+
-             '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>'+
-             '<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat</p>'+
-             '<h6>Questions to Ask Your Doctor</h6>'+
-             '<ul class="UL_layoutbox_ULColum clearfix">'+
-             '<li><span>1.</span><p>Lorem ipsum dolor sit amet</p></li><li><span>2.</span><p>Lorem ipsum dolor sit amet</p></li>'+
-             '<li><span>3.</span><p>Lorem ipsum dolor sit amet</p></li><li><span>4.</span><p>Lorem ipsum dolor sit amet</p></li>'+
-             '<li><span>5.</span><p>Lorem ipsum dolor sit amet</p></li><li><span>6.</span><p>Lorem ipsum dolor sit amet</p></li>'+
-             '</ul><a href="###" class="btn_PrintQuestions">Print Question Checklist</a></div>'+
-             '<div class="UL_layoutbox_right"><h6>Related Articles on Lactose Intolerance</h6>'+
-             '<ul class="horizontal_UL mini clearfix">'+
-             '<li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_01.jpg" alt="" /></a></div>'+
-             '<div class="rightList"><h6><a href="#">Guidelines For A Lactose Intolerance Diet</a></h6>'+
-             '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam... <a href="###">more</a></p>'+
-             '</div></li><li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_02.jpg" alt="" /></a></div>'+
-             '<div class="rightList"><h6><a href="#">Non-Dairy Milk Substitutes</a></h6>'+
-             '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam... <a href="###">more</a></p>'+
-             '</div></li><li><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_03.jpg" alt="" /></a></div>'+
-             '<div class="rightList"><h6><a href="#">Organic Soy Milk</a></h6>'+
-             '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam... <a href="###">more</a></p>'+
-             '</div></li><li class="last"><div class="leftImg"><a href="###"><img src="app/img/symptomfind/temp_04.jpg" alt="" /></a></div>'+
-             '<div class="rightList"><h6><a href="#">Wheat Allergies</a></h6>'+
-             '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam... <a href="###">more</a></p>'+
-             '</div></li></ul></div></div></div>'
+         html:'<div class="globalContBox" style=""><div class="imgGroupColumUL clearfix">'+
+             '<li><h6>Health Hazards At The Movies</h6><a href="#" class="linkImg"><img src="app/img/symptomfind/temp_12.jpg" alt="" /></a><p class="linkTxt">Signs and Symptoms Of Early Miscarriage</p></li>'+
+             '<li><h6>Getting A Flu Shot</h6><a href="#" class="linkImg"><img src="app/img/symptomfind/temp_13.jpg" alt="" /></a><p class="linkTxt">8 Germ-Filled Things In Your Home That Will Shock You</p></li>'+
+             '<li class="last"><h6>Taming Frizzy Hair</h6><a href="#" class="linkImg"><img src="app/img/symptomfind/temp_14.jpg" alt="" /></a><p class="linkTxt">Diabetic Eye Disease: How Diabetes Can Affect Your Vision</p></li>'+
+             '</div></div>'
      }]
 });
